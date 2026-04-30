bbe65bd0502fe7116970a7a622255608ef281187
Andrescg18 <chamolocoporsiempre@gmail.com>
2026-04-30 16:21:04 -0500

    Día 1: MovieNexus inicializado

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'https://image.tmdb.org/t/p',
+  apiKey: '39fc59c764c922de1458f2cd1d74a8bf'
+};
