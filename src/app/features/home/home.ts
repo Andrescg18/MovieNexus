a0da9ac103ed3602c4ceb61b0bb4caf4f4b2554b
Andrescg18 <chamolocoporsiempre@gmail.com>
2026-05-07 14:39:27 -0500

    Limpieza: Se remueve la seccion de tendencias para enfocarse en el Hero

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MovieService } from '../../core/services/movie.service';
 import { Hero } from './components/hero/hero';
-import { MovieCard } from './components/movie-card/movie-card';
 import { Movie } from '../../core/models/movie.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, MovieCard, CommonModule],
+  imports: [Hero, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
   private movieService = inject(MovieService);
   featuredMovie?: Movie;
-  trendingMovies: Movie[] = [];
 
   ngOnInit(): void {
     console.log('🎬 Home Inicializado. Cargando películas...');
@@ -24,7 +22,6 @@ export class Home implements OnInit {
         console.log('✅ ¡Éxito! Datos recibidos de TMDB:', movies);
         if (movies.length > 0) {
           this.featuredMovie = movies[0];
-          this.trendingMovies = movies.slice(1); // El resto para la lista
         }
       },
       error: (error) => {
