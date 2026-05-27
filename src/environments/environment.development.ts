de9041814bee74fa84bc14e88ae3d8ae1c40646d
Andrescg18 <chamolocoporsiempre@gmail.com>
2026-05-27 14:43:19 -0500

    Agregar script pre-build set-env.js y configurar package.json para inyeccion de API_KEY dinamica en Vercel

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://api.themoviedb.org/3',
-  imageUrl: 'https://image.tmdb.org/t/p',
-  apiKey: '39fc59c764c922de1458f2cd1d74a8bf'
-};
